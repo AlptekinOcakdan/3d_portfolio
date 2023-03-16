2db2cbba67ccb3955a7c57da59b8da3906d37bd3
alptekin <alptekin.ocakdan@gmail.com>
2023-03-16 15:34:55 +0300

    Computers component diff looking.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react'
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+    const computer = useGLTF('./desktop_pc/scene.gltf')
+    return (
+        <mesh>
+            <hemisphereLight intensity={0.15} groundColor='black'/>
+            <pointLight intensity={1}/>
+            <primitive
+                object={computer.scene}
+                scale={0.7}
+                position={[0, -3, -2.2]}
+                rotation={[-0.01, -0.2, -0.1]}
+            />
+        </mesh>
+    );
+};
+const ComputerCanvas = () => {
+    return (
+        <Canvas
+            frameloop='demand'
+            shadows
+            dpr={[1, 2]}
+            camera={{position: [20, 3, 5], fov: 25}}
+            gl={{preserveDrawingBuffer: true}}
+        >
+            <Suspense fallback={<CanvasLoader/>}>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+            </Suspense>
+        </Canvas>
+    );
+};
 
 export default Computers
