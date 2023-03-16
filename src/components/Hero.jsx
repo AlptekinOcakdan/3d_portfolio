a85cca8c6dfdd68808c6d907100bac9eb32361ce
alptekin <alptekin.ocakdan@gmail.com>
2023-03-16 21:28:19 +0300

    Corrections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,13 +15,11 @@ const Hero = () => {
                 </div>
 
                 <div>
-                    <h1 className={`${styles.heroHeadText} text-white`}>
-                        Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-                    </h1>
+                    <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span
+                        className="text-[#915eff]">Alptekin</span></h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I develop 3D visuals, user <br className='sm:block hidden'/>
-                        interfaces and web applications
-                    </p>
+                        I'm a computer engineering student specializing in web development and proficient in React,
+                        Angular, ASP.NET Core, and Node.js.</p>
                 </div>
             </div>
 
